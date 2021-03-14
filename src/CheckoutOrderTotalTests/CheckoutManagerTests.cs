22399e5d466e43b089fafddd2393a641137870dc
Josh J - Desktop <jjosephttg@gmail.com>
2021-03-14 17:28:52 -0400

    Test for Weighted Items
    Also cleaned up reused code

--- src/CheckoutOrderTotalTests/CheckoutManagerTests.cs
+++ src/CheckoutOrderTotalTests/CheckoutManagerTests.cs
@@ -6,29 +6,39 @@ namespace CheckoutOrderTotalTests {
 
         #region ConfigureItemPrice Tests
         [Test]
-        public void ConfiguringItemProvidesCorrectTotalWhenAdded() {
-            CheckoutOrderManager checkoutManager = new CheckoutOrderManager();
-
-            // Configure item
-            string groceryItem = "beef";
-            checkoutManager.SetProductUnitPrice(groceryItem, 5.87);
+        [TestCase("beef", 5.87)]
+        public void ConfiguringItemProvidesCorrectTotalWhenAdded(string groceryItem, double unitPrice) {
+            var checkoutManager = SetupCheckoutManager(groceryItem, unitPrice);
             
-            //Test that scanning an item goes through and reads in our price we set
             Assert.True(checkoutManager.ScanItem(groceryItem));
-            Assert.AreEqual(5.87, checkoutManager.GetTotalPrice());
+            Assert.AreEqual(unitPrice, checkoutManager.GetTotalPrice());
+        }
+
+        [Test]
+        [TestCase("rice", 8.3, 2.5)]
+        //For weighted items, seems like it is similar to per-unit because it is per-unit of the item, but represented as a different standard like pounds or ounces
+        public void ConfiguringWeightedItemProvidesCorrectTotalWhenAdded(string groceryItem, double unitPrice, double weight) {
+            var checkoutManager = SetupCheckoutManager(groceryItem, unitPrice);
+            
+            Assert.True(checkoutManager.ScanItem(groceryItem, weight));
+            Assert.AreEqual(unitPrice * weight, checkoutManager.GetTotalPrice());
         }
         #endregion
 
         #region ScanItem Tests
         [Test]
         public void AddingItemNotConfiguredDoesNotScanItem() {
-            CheckoutOrderManager checkoutManager = new CheckoutOrderManager();
-
-            checkoutManager.SetProductUnitPrice("milk", 5.27);
+            var checkoutManager = SetupCheckoutManager("milk", 5.27);
 
             Assert.False(checkoutManager.ScanItem("yogurt"));
             Assert.AreEqual(0, checkoutManager.GetTotalPrice());
         }
         #endregion
+
+        private CheckoutOrderManager SetupCheckoutManager(string groceryItem, double unitPrice) {
+            var checkoutManager = new CheckoutOrderManager();
+            checkoutManager.SetProductUnitPrice(groceryItem, unitPrice);
+            return checkoutManager;
+        }
     }
 }
