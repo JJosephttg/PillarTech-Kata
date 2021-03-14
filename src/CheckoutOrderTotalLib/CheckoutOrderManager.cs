bc50e807282c6a3b09e2de3cd42bc379ba2f7bef
Josh J - Desktop <jjosephttg@gmail.com>
2021-03-14 18:03:34 -0400

    Added Ability to Remove Scanned Item

--- src/CheckoutOrderTotalLib/CheckoutOrderManager.cs
+++ src/CheckoutOrderTotalLib/CheckoutOrderManager.cs
@@ -14,9 +14,7 @@ namespace CheckoutOrderTotalLib {
         Dictionary<string, double> _groceryPriceMap = new Dictionary<string, double>();
         List<GroceryItemOrder> _checkoutOrder = new List<GroceryItemOrder>();
 
-        public void SetProductUnitPrice(string groceryItem, double price) {
-            _groceryPriceMap[groceryItem] = price;
-        }
+        public void SetProductUnitPrice(string groceryItem, double price) => _groceryPriceMap[groceryItem] = price;
 
         public bool ScanItem(string groceryItem, double weightOrQty = 1) {
             if (_groceryPriceMap.TryGetValue(groceryItem, out double price)) {
@@ -26,6 +24,8 @@ namespace CheckoutOrderTotalLib {
             return false;
         }
 
-        public double GetTotalPrice() { return _checkoutOrder.Sum(x => x.Price); }
+        public void RemoveScannedItem(string groceryItem) => _checkoutOrder.Remove(_checkoutOrder.LastOrDefault(x => x.Name == groceryItem));
+
+        public double GetTotalPrice() => _checkoutOrder.Sum(x => x.Price);
     }
 }
