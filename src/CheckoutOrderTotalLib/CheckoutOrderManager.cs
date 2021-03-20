8cf1786269c6ca1a7beb66028cb373fc899b4bc6
Josh J - Desktop <jjosephttg@gmail.com>
2021-03-20 15:48:13 -0400

    Implemented Markdown and Refactored Order/Price Config Mapping is Done

--- src/CheckoutOrderTotalLib/CheckoutOrderManager.cs
+++ src/CheckoutOrderTotalLib/CheckoutOrderManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Collections.Generic;
+using System;
 
 namespace CheckoutOrderTotalLib {
    /* Expected API Calls:
@@ -11,21 +12,46 @@ namespace CheckoutOrderTotalLib {
     * RemoveProduct(item)
     */
     public class CheckoutOrderManager {
-        Dictionary<string, double> _groceryPriceMap = new Dictionary<string, double>();
-        List<GroceryItemOrder> _checkoutOrder = new List<GroceryItemOrder>();
+        // Separate checkout from price config for efficiency purposes when calculating total (Think about the 1000's of inventory a store has but how little a customer actually orders)
+        Dictionary<string, GroceryItem> _groceryPriceMap = new Dictionary<string, GroceryItem>();
+        HashSet<GroceryItem> _checkoutOrder = new HashSet<GroceryItem>();
 
-        public void SetProductUnitPrice(string groceryItem, double price) => _groceryPriceMap[groceryItem] = price;
+        public void SetProductUnitPrice(string itemId, double price) {
+            var groceryItem = TryGetOrAdd(_groceryPriceMap, itemId, new GroceryItem());
+            groceryItem.UnitPrice = price;
+        }
 
-        public bool ScanItem(string groceryItem, double weightOrQty = 1) {
-            if (_groceryPriceMap.TryGetValue(groceryItem, out double price)) {
-                _checkoutOrder.Add(new GroceryItemOrder(groceryItem, price * weightOrQty));
+        public void SetMarkdown(string itemId, double markdown) {
+            var groceryItem = TryGetOrAdd(_groceryPriceMap, itemId, new GroceryItem());
+            groceryItem.MarkDownPrice = markdown;
+        }
+
+        public bool ScanItem(string itemId, double weightOrQty = 1) {
+            if (_groceryPriceMap.TryGetValue(itemId, out GroceryItem gItem)) {
+                _checkoutOrder.Add(gItem);
+                gItem.OrderQuantity += weightOrQty;
+
                 return true;
             }
             return false;
         }
 
-        public void RemoveScannedItem(string groceryItem) => _checkoutOrder.Remove(_checkoutOrder.LastOrDefault(x => x.Name == groceryItem));
+        public void RemoveScannedItem(string itemId) {
+            if (_groceryPriceMap.TryGetValue(itemId, out GroceryItem groceryItem)) {
+                var quantity = groceryItem.OrderQuantity;
+                quantity = quantity - 1 <= 0 ? 0 : --quantity;
 
-        public double GetTotalPrice() => _checkoutOrder.Sum(x => x.Price);
+                if (quantity <= 0) _checkoutOrder.Remove(groceryItem); //Remove if exists in checkout order since customer is not ordering this item any more
+                
+                groceryItem.OrderQuantity = quantity;
+            }    
+        }
+
+        public double GetTotalPrice() => _checkoutOrder.Sum(x => x.GetAdjustedPrice());
+
+        private V TryGetOrAdd<K, V>(Dictionary<K, V> map, K key, V newVal) {
+            if (!map.TryGetValue(key, out V val)) map.Add(key, val = newVal);
+            return val;
+        }
     }
 }
---
+++ src/CheckoutOrderTotalLib/ItemSpecifications/GroceryItem.cs
@@ -0,0 +1,9 @@
+﻿namespace CheckoutOrderTotalLib {
+    internal class GroceryItem {
+        public double UnitPrice { get; set; }
+        public double MarkDownPrice { get; set; }
+        public double OrderQuantity { get; set; }
+
+        public double GetAdjustedPrice() => (UnitPrice - MarkDownPrice) * OrderQuantity;
+    }
+}
