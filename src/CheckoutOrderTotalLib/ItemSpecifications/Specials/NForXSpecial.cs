6d510cfe21b584a697e6118a9e63b38a5d5ba98c
Josh J - Desktop <jjosephttg@gmail.com>
2021-04-03 17:14:51 -0400

    Implemented N For X Special Limits

--- src/CheckoutOrderTotalLib/ItemSpecifications/Specials/NForXSpecial.cs
+++ src/CheckoutOrderTotalLib/ItemSpecifications/Specials/NForXSpecial.cs
@@ -1,12 +1,21 @@
 ﻿namespace CheckoutOrderTotalLib {
     public class NForXSpecial : ISpecial {
         readonly double _qualifiedQty, _discountPrice;
-        public NForXSpecial(double qualifiedQty, double discountPrice) {
+        readonly int _limit;
+        public NForXSpecial(double qualifiedQty, double discountPrice, int limit = 1) {
             _qualifiedQty = qualifiedQty;
             _discountPrice = discountPrice;
+            _limit = limit;
         }
 
-        SpecialResult ISpecial.Apply(GroceryItem groceryItem) => 
-            groceryItem.OrderQuantity >= _qualifiedQty ? new SpecialResult(_discountPrice, groceryItem.OrderQuantity - _qualifiedQty) : new SpecialResult(0, groceryItem.OrderQuantity);
+        SpecialResult ISpecial.Apply(GroceryItem groceryItem) {
+            double totalQtyLeft = groceryItem.OrderQuantity, total = 0;
+
+            for (int limit = 0; totalQtyLeft - _qualifiedQty >= 0 && limit < _limit; limit++) {
+                total += _discountPrice;
+                totalQtyLeft -= _qualifiedQty;
+            }
+            return new SpecialResult(total, totalQtyLeft);
+        }
     }
 }
