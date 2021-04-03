0b3d5c017b9fe40fb06cdd36231e5a2fc09607d4
Josh J - Desktop <jjosephttg@gmail.com>
2021-04-03 17:55:38 -0400

    Removed Comments

--- src/CheckoutOrderTotalLib/ItemSpecifications/Specials/NForXSpecial.cs
+++ src/CheckoutOrderTotalLib/ItemSpecifications/Specials/NForXSpecial.cs
@@ -9,10 +9,7 @@
         internal override SpecialResult Apply(GroceryItem groceryItem) {
             double orderQty = groceryItem.OrderQuantity;
             int maxLimitAmt = (int)(orderQty / _qualifiedQty);
-            // Get the limit of how many times we can apply the special (Either our order quantity is small enough to be under our limit, or the limit is beyond what we have ordered)
             var limit = maxLimitAmt > _limit ? _limit : maxLimitAmt;
-            //Multiply our calculated limit by the discount price to get the total applied special price
-            //For the quantity it will be our total quantity minus what we applied to the special (Calculated limit * the quantity that qualified for it)
             return new SpecialResult(limit * _discountPrice, orderQty - limit * _qualifiedQty);
         }
     }
