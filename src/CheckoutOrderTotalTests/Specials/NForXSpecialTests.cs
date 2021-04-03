c5d63c0b2057cf7a19dd270c7d5da2066a8077ca
Josh J - Desktop <jjosephttg@gmail.com>
2021-04-03 16:42:07 -0400

    Created Test for N For X Special

---
+++ src/CheckoutOrderTotalTests/Specials/NForXSpecialTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace CheckoutOrderTotalTests {
+    public class NForXSpecialTests : GroceryPOSSystemTestBase {
+        [Test]
+        [TestCase(3, 3, 5, 5)]
+        [TestCase(5, 4, 7.5, 57.5)]
+        public void SpecialAppliesToTotalWhenMet(double orderQty, double qualifiedQty, double discountPrice, double expectedPrice) {
+            var checkoutManager = SetupAndScan(C_DefaultItem, C_DefaultUnitPrice, orderQty);
+            checkoutManager.SetSpecial(C_DefaultItem, new NForXSpecial(qualifiedQty, discountPrice));
+
+            Assert.AreEqual(expectedPrice, checkoutManager.GetTotalPrice());
+        }
+    }
+}
